02e544d9a0537f0c19cd691d5f6286a8eca7604a
Victor Palmero <117776704+palmerovicdev@users.noreply.github.com>
2024-04-22 11:00:39 -0400

    feat: create model entity

---
+++ src/main/java/com/palmerovicdev/astroinitializer/model/AstroModuleEntity.java
@@ -0,0 +1,76 @@
+package com.palmerovicdev.astroinitializer.model;
+
+public class AstroModuleEntity {
+    private String templateName;
+    private String packageManager;
+    private boolean installDependencies;
+    private boolean createGitRepo;
+    private boolean useTypescript;
+    private String typescriptMode;
+    private boolean useTemplate;
+    private boolean unicodeSupport;
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public String getPackageManager() {
+        return packageManager;
+    }
+
+    public void setPackageManager(String packageManager) {
+        this.packageManager = packageManager;
+    }
+
+    public boolean isInstallDependencies() {
+        return installDependencies;
+    }
+
+    public void setInstallDependencies(boolean installDependencies) {
+        this.installDependencies = installDependencies;
+    }
+
+    public boolean isCreateGitRepo() {
+        return createGitRepo;
+    }
+
+    public void setCreateGitRepo(boolean createGitRepo) {
+        this.createGitRepo = createGitRepo;
+    }
+
+    public boolean isUseTypescript() {
+        return useTypescript;
+    }
+
+    public void setUseTypescript(boolean useTypescript) {
+        this.useTypescript = useTypescript;
+    }
+
+    public String getTypescriptMode() {
+        return typescriptMode;
+    }
+
+    public void setTypescriptMode(String typescriptMode) {
+        this.typescriptMode = typescriptMode;
+    }
+
+    public boolean isUseTemplate() {
+        return useTemplate;
+    }
+
+    public void setUseTemplate(boolean useTemplate) {
+        this.useTemplate = useTemplate;
+    }
+
+    public boolean isUnicodeSupport() {
+        return unicodeSupport;
+    }
+
+    public void setUnicodeSupport(boolean unicodeSupport) {
+        this.unicodeSupport = unicodeSupport;
+    }
+}
